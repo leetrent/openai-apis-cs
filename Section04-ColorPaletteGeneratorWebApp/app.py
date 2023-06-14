051780c053f4d7dadfc9733f8ed90516df94a8ae
LeeTrent <lee.trent.1@gmail.com>
2023-06-13 20:31:58 -0500

    Working on color palette web app

--- Section04-ColorPaletteGeneratorWebApp/app.py
+++ Section04-ColorPaletteGeneratorWebApp/app.py
@@ -1,13 +1,24 @@
+import openai
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def index():
-    return "Hello there from Flask!!!"
+    createResult = openai.Completion.create(
+        model="text-davinci-003",
+        max_tokens=200,
+        prompt="Make me laugh!"
+    )
+    return(createResult["choices"][0]["text"])
+    #return render_template("index.html")
 
-@app.route("/pooh")
-def pooh():
+@app.route("/palette", methods=["POST"])
+def palette():
     return "Hello there P. Bear"
 
 if __name__ == "__main__":
